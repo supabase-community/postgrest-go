54d7cc261a246c4230dd36aba45d442ace5d4d3e
Yusuf Papurcu <yusufturhanp@gmail.com>
2021-05-24 20:20:25 +0300

    (dev:test): rpc basic test added

---
+++ test/rpc/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/muratmirgun/postgrest-go"
+)
+
+var (
+	REST_URL = `http://localhost:3000`
+)
+
+func main() {
+	client := postgrest.NewClient(REST_URL, "", nil)
+	result, err := client.Rpc("add_them", "", map[string]int{"a": 9, "b": 3})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
+}
