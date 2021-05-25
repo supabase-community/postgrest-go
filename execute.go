98499c7bdb026ea2869c4402519a1e4b5393ff90
Yusuf Papurcu <yusufturhanp@gmail.com>
2021-05-25 18:43:54 +0300

    (dev): added execute function

--- execute.go
+++ execute.go
@@ -1,5 +1,33 @@
 package postgrest
 
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
 func Execute(client *Client, method string, body []byte) (string, error) {
-	return "", nil
+	readerBody := bytes.NewBuffer(body)
+	req, err := http.NewRequest(method, client.clientTransport.baseURL.Path, readerBody)
+	if err != nil {
+		return "", err
+	}
+	resp, err := client.session.Do(req)
+	if err != nil {
+		return "", err
+	}
+	fmt.Println(resp.Request.URL.String())
+	respbody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	result := string(respbody)
+
+	err = resp.Body.Close()
+	if err != nil {
+		return "", err
+	}
+	return result, nil
 }
