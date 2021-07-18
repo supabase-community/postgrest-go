2e0084c9f8b89804caf2f12db793802a9b59a306
Mark Lussier <mlussier@gmail.com>
2021-07-18 06:22:17 -0600

    in cases of non 20 responses, actually return an errpr from executeHelper

--- execute.go
+++ execute.go
@@ -3,9 +3,20 @@ package postgrest
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 )
+
+// ExecuteErrorResponse is the error response format from postgrest. We really
+// only use Code and Message, but we'll keep it as a struct for now.
+
+type ExecuteErrorResponse struct {
+	Hint    string `json:"hint"`
+	Details string `json:"details"`
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
 
 func executeHelper(client *Client, method string, body []byte) ([]byte, error) {
 	if client.ClientError != nil {
@@ -17,14 +28,24 @@ func executeHelper(client *Client, method string, body []byte) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	resp, err := client.session.Do(req)
 	if err != nil {
 		return nil, err
 	}
 
-	respbody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
+	respbody, rerr := io.ReadAll(resp.Body)
+	if rerr != nil {
+		return nil, rerr
+	}
+
+	// If we didnt get 20x response, unmarshal the error body to be able to format
+	// an informative error message. Mayb this should be >= 400, since I doubt redirect
+	// errors are returned.. but to be safe
+	if resp.StatusCode >= 300 {
+		var errmsg *ExecuteErrorResponse
+		json.Unmarshal(respbody, &errmsg)
+		return nil, fmt.Errorf("(%s) %s", errmsg.Code, errmsg.Message)
 	}
 
 	err = resp.Body.Close()
