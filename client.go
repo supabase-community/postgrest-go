8440d1f29fc328e00a96540ebe79e85bc8e3293c
Yusuf Papurcu <yusufturhanp@gmail.com>
2022-01-25 18:19:07 +0300

    fix(client/auth): Auth changed to Bearer

--- client.go
+++ client.go
@@ -58,7 +58,7 @@ type Client struct {
 
 // TokenAuth sets authorization headers for subsequent requests.
 func (c *Client) TokenAuth(token string) *Client {
-	c.clientTransport.header.Set("Authorization", "Basic "+token)
+	c.clientTransport.header.Set("Authorization", "Bearer "+token)
 	c.clientTransport.header.Set("apikey", token)
 	return c
 }
