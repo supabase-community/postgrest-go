847cc1feec1f5a21c517691b1922f8a953e97a79
Ron Jones <ron@ovao.dev>
2022-01-28 12:35:28 -0800

    Add FilterBuilder tests; doc comments

--- filterbuilder_test.go
+++ filterbuilder_test.go
@@ -1,7 +1,9 @@
 package postgrest
 
 import (
+	"encoding/json"
 	"net/http"
+	"sort"
 	"testing"
 
 	"github.com/jarcoal/httpmock"
@@ -86,4 +88,157 @@ func ExampleFilterBuilder_ExecuteTo() {
 		// The value for res will contain all columns for all users, and count will
 		// be the exact number of rows in the users table.
 	}
+}
+
+func TestFilterBuilder_Limit(t *testing.T) {
+	c := createClient(t)
+	assert := assert.New(t)
+
+	want := []map[string]interface{}{users[0]}
+	got := []map[string]interface{}{}
+
+	if mockResponses {
+		httpmock.Activate()
+		defer httpmock.DeactivateAndReset()
+
+		httpmock.RegisterRegexpResponder("GET", mockPath, func(req *http.Request) (*http.Response, error) {
+			resp, _ := httpmock.NewJsonResponse(200, want)
+			resp.Header.Add("Content-Range", "*/2")
+			return resp, nil
+		})
+	}
+
+	bs, count, err := c.From("users").Select("id, name, email", "exact", false).Limit(1, "").Execute()
+	assert.NoError(err)
+
+	err = json.Unmarshal(bs, &got)
+	assert.NoError(err)
+	assert.EqualValues(want, got)
+
+	// Matching supabase-js, the count returned is not the number of transformed
+	// rows, but the number of filtered rows.
+	assert.Equal(countType(len(users)), count, "expected count to be %v", len(users))
+}
+
+func TestFilterBuilder_Order(t *testing.T) {
+	c := createClient(t)
+	assert := assert.New(t)
+
+	want := make([]map[string]interface{}, len(users))
+	copy(want, users)
+
+	sort.Slice(want, func(i, j int) bool {
+		return j < i
+	})
+
+	got := []map[string]interface{}{}
+
+	if mockResponses {
+		httpmock.Activate()
+		defer httpmock.DeactivateAndReset()
+
+		httpmock.RegisterRegexpResponder("GET", mockPath, func(req *http.Request) (*http.Response, error) {
+			resp, _ := httpmock.NewJsonResponse(200, want)
+			resp.Header.Add("Content-Range", "*/2")
+			return resp, nil
+		})
+	}
+
+	bs, count, err := c.
+		From("users").
+		Select("id, name, email", "exact", false).
+		Order("name", &OrderOpts{Ascending: true}).
+		Execute()
+	assert.NoError(err)
+
+	err = json.Unmarshal(bs, &got)
+	assert.NoError(err)
+	assert.EqualValues(want, got)
+	assert.Equal(countType(len(users)), count)
+}
+
+func TestFilterBuilder_Range(t *testing.T) {
+	c := createClient(t)
+	assert := assert.New(t)
+
+	want := users
+	got := []map[string]interface{}{}
+
+	if mockResponses {
+		httpmock.Activate()
+		defer httpmock.DeactivateAndReset()
+
+		httpmock.RegisterRegexpResponder("GET", mockPath, func(req *http.Request) (*http.Response, error) {
+			resp, _ := httpmock.NewJsonResponse(200, want)
+			resp.Header.Add("Content-Range", "*/2")
+			return resp, nil
+		})
+	}
+
+	bs, count, err := c.
+		From("users").
+		Select("id, name, email", "exact", false).
+		Range(0, 1, "").
+		Execute()
+	assert.NoError(err)
+
+	err = json.Unmarshal(bs, &got)
+	assert.NoError(err)
+	assert.EqualValues(want, got)
+	assert.Equal(countType(len(users)), count)
+}
+
+func TestFilterBuilder_Single(t *testing.T) {
+	c := createClient(t)
+	assert := assert.New(t)
+
+	want := users[0]
+	got := make(map[string]interface{})
+
+	t.Run("ValidResult", func(t *testing.T) {
+		if mockResponses {
+			httpmock.Activate()
+			defer httpmock.DeactivateAndReset()
+
+			httpmock.RegisterRegexpResponder("GET", mockPath, func(req *http.Request) (*http.Response, error) {
+				resp, _ := httpmock.NewJsonResponse(200, want)
+				resp.Header.Add("Content-Range", "*/2")
+				return resp, nil
+			})
+		}
+
+		bs, count, err := c.
+			From("users").
+			Select("id, name, email", "exact", false).
+			Limit(1, "").
+			Single().
+			Execute()
+		assert.NoError(err)
+
+		err = json.Unmarshal(bs, &got)
+		assert.NoError(err)
+		assert.EqualValues(want, got)
+		assert.Equal(countType(len(users)), count)
+	})
+
+	// An error will be returned from PostgREST if the total count of the result
+	// set > 1, so Single can pretty easily err.
+	t.Run("Error", func(t *testing.T) {
+		if mockResponses {
+			httpmock.Activate()
+			defer httpmock.DeactivateAndReset()
+
+			httpmock.RegisterRegexpResponder("GET", mockPath, func(req *http.Request) (*http.Response, error) {
+				resp, _ := httpmock.NewJsonResponse(500, ExecuteError{
+					Message: "error message",
+				})
+
+				resp.Header.Add("Content-Range", "*/2")
+				return resp, nil
+			})
+		}
+
+		_, _, err := c.From("users").Select("*", "", false).Single().Execute()
+		assert.Error(err)
+	})
 }
